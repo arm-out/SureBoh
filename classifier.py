2c28756ba82b2163a245d9cc2afbf64c95a71d57
Armin <arminsuraj@gmail.com>
2019-08-17 00:34:09 +0800

    finished classifier

--- classifier.py
+++ classifier.py
@@ -2,6 +2,23 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn import metrics
+import itertools
 
 df = pd.read_pickle('Data/df1.pickle')
-print(df.head())
+y = df.label
+df.drop('label', axis=1, inplace=True)
+
+X_train, X_test, y_train, y_test = train_test_split(df.text, y, test_size=0.33)
+
+tfidf_vectorizer = TfidfVectorizer(stop_words='english', max_df=0.8)
+tfidf_train = tfidf_vectorizer.fit_transform(X_train)
+tfidf_test = tfidf_vectorizer.transform(X_test)
+
+clf = MultinomialNB()
+clf.fit(tfidf_train, y_train)
+pred = clf.predict(tfidf_test)
+score = metrics.accuracy_score(y_test, pred)
+
+print ("accuracy:   %0.3f" % score)
