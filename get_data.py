eadeb1c10b940d22e958e974581b7f04dbfbbdc6
Armin <arminsuraj@gmail.com>
2019-08-16 23:46:45 +0800

    get df2

--- get_data.py
+++ get_data.py
@@ -4,12 +4,39 @@ import pickle
 def grab_df1():
     #grabbing dataset1
     df = pd.read_csv("https://s3.amazonaws.com/assets.datacamp.com/blog_assets/fake_or_real_news.csv")
-    df = df.drop("Unnamed: 0", axis = 1)
-    print(df.head)
+    df = df.drop("Unnamed: 0", axis=1)
+    #print(df.head)
+    #df.label.value_counts()
 
     #pickling def
     pickle_out = open('df1.pickle', 'wb')
     pickle.dump(df, pickle_out)
     pickle_out.close()
 
-grab_df1()
+def grab_df2():
+    #grabbing dataset2
+    df = pd.read_csv("fake.csv")
+
+    l = []
+    for i in range(len(df.text)):
+        l.append(len(str(df.text[i])))
+
+    l = pd.DataFrame(l)
+    df['textlen'] = l
+
+    df = df[df.language == 'english']
+    df = df[(df.textlen > 500) & (df.textlen < 12000)]
+    df = df[['site_url', 'title', 'text']]
+    df.reset_index(drop = 'index', inplace=True)
+    df.drop('site_url', axis=1, inplace=True)
+    df['label'] = 'FAKE'
+
+    #pickling df2
+    pickle_out = open('df2.pickle', 'wb')
+    pickle.dump(df, pickle_out)
+    pickle_out.close()
+    
+
+
+#grab_df1()
+grab_df2()
